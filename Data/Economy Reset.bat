b47bceb0e42a496690d5f9f8eb6a28c8f6965635
Bas van Driel <darkslayerzz@hotmail.com>
2012-02-17 11:15:31 +0100

    l

---
+++ Data/Economy Reset.bat
@@ -0,0 +1,5 @@
+@echo off
+Title Economy Reseter
+"C:\Program Files\Java\jdk1.6.0_26\bin\javac.exe" EconomyReset.java
+java EconomyReset
+pause 
---
+++ Initiate Server.bat
@@ -0,0 +1,4 @@
+@echo off
+title Xyle
+"C:\Program Files\Java\jdk1.7.0\bin\java.exe" -Xmx1024m -cp bin;deps/poi.jar;deps/mysql.jar;deps/mina.jar;deps/slf4j.jar;deps/slf4j-nop.jar;deps/jython.jar;log4j-1.2.15.jar; server.Server
+pause
